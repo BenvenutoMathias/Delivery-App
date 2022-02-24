f7b001fda21e757e36f5729cea9f11e44f75280f
BenvenutoMathias <benvenutomathias65@gmail.com>
2022-02-24 17:48:18 -0300

    Layout

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,24 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿let aFood = document.getElementById('aFood');
+let aDrink = document.getElementById('aDrink');
+let aBread = document.getElementById('aBread');
 
-// Write your JavaScript code.
+let SectionFoods = document.getElementById('Foods');
+let SectionDrinks = document.getElementById('Drinks');
+let SectionBreads = document.getElementById('Breads');
+
+let hidden = true;
+
+const ChangeAttr = (element) => {
+
+    if (hidden) {
+        element.style.display = "flex";
+        hidden = false;
+    } else {
+        element.style.display = "none";
+        hidden = true;
+    }
+}
+aFood.addEventListener("click", function () { ChangeAttr(SectionFoods) });
+aDrink.addEventListener("click", function () { ChangeAttr(SectionDrinks) });
+aBread.addEventListener("click", function () { ChangeAttr(SectionBreads) });
+
