becb2f9fff1a0ac1b16430b88fa71175b0f0bdec
BenvenutoMathias <benvenutomathias65@gmail.com>
2022-02-28 20:17:43 -0300

    ContextSeedDataFolder

---
+++ Data/ContextSeed.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using DeliveryApp.Models;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace DeliveryApp.Data
+{
+    public static class ContextSeed
+    {
+
+        public static async Task SeedRolesAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            //Seed Roles
+            await roleManager.CreateAsync(new IdentityRole("SuperAdmin"));
+
+        }
+
+
+        public static async Task SeedSuperAdminAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            //Seed Default User
+            var defaultUser = new ApplicationUser
+            {
+                UserName = "superadmin",
+                Email = "superadmin@email.com",
+                FirstName = "super",
+                LastName = "admin",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true,
+            };
+            if (userManager.Users.All(u => u.Id != defaultUser.Id))
+            {
+                var user = await userManager.FindByEmailAsync(defaultUser.Email);
+
+                if (user == null)
+                {
+                    await userManager.CreateAsync(defaultUser, "Contraseña23!");
+                    await userManager.AddToRoleAsync(defaultUser, "SuperAdmin");
+                    
+
+                }
+
+            }
+        }
+    }
+}
---
+++ Data/Migrations/20220228222112_CorrectionPropertyModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DeliveryApp.Data.Migrations
+{
+    public partial class CorrectionPropertyModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Number",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Number",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
