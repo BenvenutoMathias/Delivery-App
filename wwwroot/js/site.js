260fc0998e5e2597697fcd4cdd72f2e60a4cdbfe
BenvenutoMathias <benvenutomathias65@gmail.com>
2022-03-09 22:58:24 -0300

    ResponsiveIndex

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,4 @@
-﻿let aFood = document.getElementById('aFood');
+﻿/* let aFood = document.getElementById('aFood');
 let aDrink = document.getElementById('aDrink');
 let aBread = document.getElementById('aBread');
 
@@ -21,4 +21,17 @@ const ChangeAttr = (element) => {
 aFood.addEventListener("click", function () { ChangeAttr(SectionFoods) });
 aDrink.addEventListener("click", function () { ChangeAttr(SectionDrinks) });
 aBread.addEventListener("click", function () { ChangeAttr(SectionBreads) });
+ */
+//  Declaraciones
+const d = document;
+const menuBtn = d.querySelector('#menu');
+const menuLst = d.querySelector('header .menu');
+
+//  menu hamburguesa
+menuBtn.onclick = () => {
+    let text;
+    // condicion ? valor V : valor F
+    menuLst.classList.toggle('active') ? text = '&times;' : text = '&equiv;';
+    menuBtn.innerHTML = text;
+}
 
